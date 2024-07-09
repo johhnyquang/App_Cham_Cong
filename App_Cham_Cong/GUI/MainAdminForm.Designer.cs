17685b4ef4c392ee461de21dd5273b6f7307626a
Johhny Quang <johhnydang141010@gmail.com>
2024-07-09 23:44:01 +0700

    # Commit MainAdminForm

---
+++ App_Cham_Cong/GUI/MainAdminForm.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace App_Cham_Cong.GUI
+{
+    partial class frmMainAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnMain = new System.Windows.Forms.Panel();
+            this.pnMainSub = new System.Windows.Forms.Panel();
+            this.pnInfoBOx = new System.Windows.Forms.Panel();
+            this.txtPositionEmp = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBirthdayEmp = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNameEmp = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnGreen = new System.Windows.Forms.Panel();
+            this.pnOrange = new System.Windows.Forms.Panel();
+            this.pnBlue = new System.Windows.Forms.Panel();
+            this.pnRed = new System.Windows.Forms.Panel();
+            this.pnYellow = new System.Windows.Forms.Panel();
+            this.lbNameMember = new System.Windows.Forms.Label();
+            this.picInfoBox = new System.Windows.Forms.PictureBox();
+            this.btnDoiMKForm = new System.Windows.Forms.Button();
+            this.btnGroupManagementForm = new System.Windows.Forms.Button();
+            this.btnAccountManagementForm = new System.Windows.Forms.Button();
+            this.picAvtMember = new System.Windows.Forms.PictureBox();
+            this.pnMain.SuspendLayout();
+            this.pnMainSub.SuspendLayout();
+            this.pnInfoBOx.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picInfoBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAvtMember)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnMain
+            // 
+            this.pnMain.Controls.Add(this.pnMainSub);
+            this.pnMain.Controls.Add(this.panel1);
+            this.pnMain.Location = new System.Drawing.Point(-2, 8);
+            this.pnMain.Name = "pnMain";
+            this.pnMain.Size = new System.Drawing.Size(1015, 543);
+            this.pnMain.TabIndex = 1;
+            // 
+            // pnMainSub
+            // 
+            this.pnMainSub.Controls.Add(this.pnInfoBOx);
+            this.pnMainSub.Controls.Add(this.panel2);
+            this.pnMainSub.Location = new System.Drawing.Point(-6, 98);
+            this.pnMainSub.Name = "pnMainSub";
+            this.pnMainSub.Size = new System.Drawing.Size(1020, 430);
+            this.pnMainSub.TabIndex = 1;
+            // 
+            // pnInfoBOx
+            // 
+            this.pnInfoBOx.Controls.Add(this.txtPositionEmp);
+            this.pnInfoBOx.Controls.Add(this.label4);
+            this.pnInfoBOx.Controls.Add(this.txtBirthdayEmp);
+            this.pnInfoBOx.Controls.Add(this.label3);
+            this.pnInfoBOx.Controls.Add(this.txtNameEmp);
+            this.pnInfoBOx.Controls.Add(this.label2);
+            this.pnInfoBOx.Controls.Add(this.label1);
+            this.pnInfoBOx.Controls.Add(this.picInfoBox);
+            this.pnInfoBOx.Location = new System.Drawing.Point(268, 17);
+            this.pnInfoBOx.Name = "pnInfoBOx";
+            this.pnInfoBOx.Size = new System.Drawing.Size(735, 388);
+            this.pnInfoBOx.TabIndex = 1;
+            // 
+            // txtPositionEmp
+            // 
+            this.txtPositionEmp.Enabled = false;
+            this.txtPositionEmp.Location = new System.Drawing.Point(29, 307);
+            this.txtPositionEmp.Name = "txtPositionEmp";
+            this.txtPositionEmp.Size = new System.Drawing.Size(212, 22);
+            this.txtPositionEmp.TabIndex = 10;
+            this.txtPositionEmp.Text = "Quản Lý";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(23, 269);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Chức vụ";
+            // 
+            // txtBirthdayEmp
+            // 
+            this.txtBirthdayEmp.Enabled = false;
+            this.txtBirthdayEmp.Location = new System.Drawing.Point(28, 218);
+            this.txtBirthdayEmp.Name = "txtBirthdayEmp";
+            this.txtBirthdayEmp.Size = new System.Drawing.Size(212, 22);
+            this.txtBirthdayEmp.TabIndex = 8;
+            this.txtBirthdayEmp.Text = "14/10/2003";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ngày sinh";
+            // 
+            // txtNameEmp
+            // 
+            this.txtNameEmp.Enabled = false;
+            this.txtNameEmp.Location = new System.Drawing.Point(28, 137);
+            this.txtNameEmp.Name = "txtNameEmp";
+            this.txtNameEmp.Size = new System.Drawing.Size(212, 22);
+            this.txtNameEmp.TabIndex = 6;
+            this.txtNameEmp.Text = "Trần Quang Tuấn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tên nhân viên";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 32);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Hồ sơ nhân viên";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Violet;
+            this.panel2.Controls.Add(this.btnDoiMKForm);
+            this.panel2.Controls.Add(this.btnGroupManagementForm);
+            this.panel2.Controls.Add(this.btnAccountManagementForm);
+            this.panel2.Location = new System.Drawing.Point(18, 16);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(225, 390);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pnGreen);
+            this.panel1.Controls.Add(this.pnOrange);
+            this.panel1.Controls.Add(this.pnBlue);
+            this.panel1.Controls.Add(this.pnRed);
+            this.panel1.Controls.Add(this.pnYellow);
+            this.panel1.Controls.Add(this.lbNameMember);
+            this.panel1.Controls.Add(this.picAvtMember);
+            this.panel1.Location = new System.Drawing.Point(3, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1002, 90);
+            this.panel1.TabIndex = 0;
+            // 
+            // pnGreen
+            // 
+            this.pnGreen.BackColor = System.Drawing.Color.Lime;
+            this.pnGreen.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.pnGreen.Location = new System.Drawing.Point(928, 23);
+            this.pnGreen.Name = "pnGreen";
+            this.pnGreen.Size = new System.Drawing.Size(67, 53);
+            this.pnGreen.TabIndex = 3;
+            this.pnGreen.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pnGreen_MouseClick);
+            // 
+            // pnOrange
+            // 
+            this.pnOrange.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.pnOrange.Location = new System.Drawing.Point(846, 23);
+            this.pnOrange.Name = "pnOrange";
+            this.pnOrange.Size = new System.Drawing.Size(67, 53);
+            this.pnOrange.TabIndex = 3;
+            this.pnOrange.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pnOrange_MouseClick);
+            // 
+            // pnBlue
+            // 
+            this.pnBlue.BackColor = System.Drawing.Color.Blue;
+            this.pnBlue.Location = new System.Drawing.Point(756, 23);
+            this.pnBlue.Name = "pnBlue";
+            this.pnBlue.Size = new System.Drawing.Size(67, 53);
+            this.pnBlue.TabIndex = 3;
+            this.pnBlue.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pnBlue_MouseClick);
+            // 
+            // pnRed
+            // 
+            this.pnRed.BackColor = System.Drawing.Color.Red;
+            this.pnRed.Location = new System.Drawing.Point(661, 23);
+            this.pnRed.Name = "pnRed";
+            this.pnRed.Size = new System.Drawing.Size(67, 53);
+            this.pnRed.TabIndex = 3;
+            this.pnRed.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pnRed_MouseClick);
+            // 
+            // pnYellow
+            // 
+            this.pnYellow.BackColor = System.Drawing.Color.Yellow;
+            this.pnYellow.Location = new System.Drawing.Point(564, 23);
+            this.pnYellow.Name = "pnYellow";
+            this.pnYellow.Size = new System.Drawing.Size(67, 53);
+            this.pnYellow.TabIndex = 2;
+            this.pnYellow.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pnYellow_MouseClick);
+            // 
+            // lbNameMember
+            // 
+            this.lbNameMember.AutoSize = true;
+            this.lbNameMember.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNameMember.Location = new System.Drawing.Point(109, 31);
+            this.lbNameMember.Name = "lbNameMember";
+            this.lbNameMember.Size = new System.Drawing.Size(337, 32);
+            this.lbNameMember.TabIndex = 1;
+            this.lbNameMember.Text = "Tên Nhân Viên (Thay đổi)";
+            // 
+            // picInfoBox
+            // 
+            this.picInfoBox.Image = global::App_Cham_Cong.Properties.Resources.QUANG_TUẤN;
+            this.picInfoBox.Location = new System.Drawing.Point(283, 8);
+            this.picInfoBox.Name = "picInfoBox";
+            this.picInfoBox.Size = new System.Drawing.Size(451, 379);
+            this.picInfoBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picInfoBox.TabIndex = 0;
+            this.picInfoBox.TabStop = false;
+            // 
+            // btnDoiMKForm
+            // 
+            this.btnDoiMKForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDoiMKForm.Image = global::App_Cham_Cong.Properties.Resources.icons8_change_25;
+            this.btnDoiMKForm.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnDoiMKForm.Location = new System.Drawing.Point(10, 170);
+            this.btnDoiMKForm.Name = "btnDoiMKForm";
+            this.btnDoiMKForm.Size = new System.Drawing.Size(200, 51);
+            this.btnDoiMKForm.TabIndex = 4;
+            this.btnDoiMKForm.Text = "Đổi mật khẩu";
+            this.btnDoiMKForm.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDoiMKForm.UseVisualStyleBackColor = true;
+            // 
+            // btnGroupManagementForm
+            // 
+            this.btnGroupManagementForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGroupManagementForm.Image = global::App_Cham_Cong.Properties.Resources.icons8_decentralization_25;
+            this.btnGroupManagementForm.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnGroupManagementForm.Location = new System.Drawing.Point(10, 95);
+            this.btnGroupManagementForm.Name = "btnGroupManagementForm";
+            this.btnGroupManagementForm.Size = new System.Drawing.Size(200, 51);
+            this.btnGroupManagementForm.TabIndex = 1;
+            this.btnGroupManagementForm.Text = "Phân quyền chức vụ";
+            this.btnGroupManagementForm.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGroupManagementForm.UseVisualStyleBackColor = true;
+            // 
+            // btnAccountManagementForm
+            // 
+            this.btnAccountManagementForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAccountManagementForm.Image = global::App_Cham_Cong.Properties.Resources.icons8_sign_up_25;
+            this.btnAccountManagementForm.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnAccountManagementForm.Location = new System.Drawing.Point(10, 16);
+            this.btnAccountManagementForm.Name = "btnAccountManagementForm";
+            this.btnAccountManagementForm.Size = new System.Drawing.Size(200, 51);
+            this.btnAccountManagementForm.TabIndex = 0;
+            this.btnAccountManagementForm.Text = "Đăng ký tải khoản";
+            this.btnAccountManagementForm.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAccountManagementForm.UseVisualStyleBackColor = true;
+            // 
+            // picAvtMember
+            // 
+            this.picAvtMember.Location = new System.Drawing.Point(26, 17);
+            this.picAvtMember.Name = "picAvtMember";
+            this.picAvtMember.Size = new System.Drawing.Size(66, 59);
+            this.picAvtMember.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picAvtMember.TabIndex = 0;
+            this.picAvtMember.TabStop = false;
+            // 
+            // frmMainAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1010, 558);
+            this.Controls.Add(this.pnMain);
+            this.Name = "frmMainAdmin";
+            this.Text = "MainAdminForm";
+            this.Load += new System.EventHandler(this.frmMainAdmin_Load);
+            this.pnMain.ResumeLayout(false);
+            this.pnMainSub.ResumeLayout(false);
+            this.pnInfoBOx.ResumeLayout(false);
+            this.pnInfoBOx.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picInfoBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAvtMember)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnMain;
+        private System.Windows.Forms.Panel pnMainSub;
+        private System.Windows.Forms.Panel pnInfoBOx;
+        private System.Windows.Forms.TextBox txtPositionEmp;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBirthdayEmp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNameEmp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picInfoBox;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnDoiMKForm;
+        private System.Windows.Forms.Button btnGroupManagementForm;
+        private System.Windows.Forms.Button btnAccountManagementForm;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnGreen;
+        private System.Windows.Forms.Panel pnOrange;
+        private System.Windows.Forms.Panel pnBlue;
+        private System.Windows.Forms.Panel pnRed;
+        private System.Windows.Forms.Panel pnYellow;
+        private System.Windows.Forms.Label lbNameMember;
+        private System.Windows.Forms.PictureBox picAvtMember;
+    }
+}
---
+++ App_Cham_Cong/GUI/MainAdminForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_Cham_Cong.GUI
+{
+    public partial class frmMainAdmin : Form
+    {
+        public frmMainAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMainAdmin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pnYellow_MouseClick(object sender, MouseEventArgs e)
+        {
+            pnMainSub.BackColor = Color.LightGoldenrodYellow;
+        }
+
+        private void pnRed_MouseClick(object sender, MouseEventArgs e)
+        {
+            pnMainSub.BackColor= Color.OrangeRed;
+        }
+
+        private void pnBlue_MouseClick(object sender, MouseEventArgs e)
+        {
+            pnMainSub.BackColor = Color.Blue;
+        }
+
+        private void pnOrange_MouseClick(object sender, MouseEventArgs e)
+        {
+            pnMainSub.BackColor = Color.Orange;
+        }
+
+        private void pnGreen_MouseClick(object sender, MouseEventArgs e)
+        {
+            pnMainSub.BackColor = Color.ForestGreen;
+        }
+    }
+}
